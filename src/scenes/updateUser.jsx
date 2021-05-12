c4e1b7c6de383dfaf8a2c11cf7ff6a1bc490839f
jbro129 <jbro129fan@gmail.com>
2021-05-12 11:26:34 -0400

    Added icon upload feature

--- src/scenes/updateUser.jsx
+++ src/scenes/updateUser.jsx
@@ -1,8 +1,28 @@
 import React, {useContext, useState, useEffect} from 'react'
-import {Button, Card, Col, Form, Row} from "antd";
+import {Button, Card, Col, Form, Row, Upload, message} from "antd";
+import {LoadingOutlined, PlusOutlined} from '@ant-design/icons';
 import {AntdInput, AntdSelect, AntdTextArea} from "../components/antdMappedComponents/antdMapper";
 import {UserContext} from "../App";
 import {skills} from "../global/referenceData";
+
+
+const getBase64= (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+  
+  function beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  }
 
 export const layout = {
     labelCol: { span: 6 },
@@ -33,9 +53,41 @@ const submitForm = (fields, userId, setUserInfo) => {
 
 const UpdateUser = () => {
 
+    const [loading, setLoading] = useState(false)
     const [fields, setFields] = useState()
     const {userInfo, setUserInfo} = useContext(UserContext)
     const [form] = Form.useForm()
+
+
+    const handleChange = info => {
+        console.log('changing info', info.file.status)
+        // if (info.file.status === 'uploading') {
+        //     console.log('uploading', info.file.status)
+        //   setLoading(true)
+        //   return;
+        // }
+        // if (info.file.status === 'done') {
+        //     console.log('done', info.file.status)
+        //   // Get this url from response in real world.
+        //   getBase64(info.file.originFileObj, image => {
+        //     setUserInfo({...userInfo, userImage: image})
+        //     setLoading(false)
+        //     }
+        //   )
+        // }
+        getBase64(info.file.originFileObj, image => {
+            setUserInfo({...userInfo, userImage: image})
+            setLoading(false)
+            }
+          )
+      }
+
+    const uploadButton = (
+        <div>
+          {loading ? <LoadingOutlined /> : <PlusOutlined />}
+          <div style={{ marginTop: 8 }}>Upload</div>
+        </div>
+      );
 
     useEffect(() => {
         if (userInfo && (userInfo.id !== undefined)) {
@@ -59,6 +111,16 @@ const UpdateUser = () => {
                         >
                             <Row justify="space-around">
                                 <Col span={20}>
+                                <Upload
+                                    name="avatar"
+                                    listType="picture-card"
+                                    className="avatar-uploader"
+                                    showUploadList={false}
+                                    beforeUpload={beforeUpload}
+                                    onChange={handleChange}
+                                >
+                                    {userInfo.userImage ? <img src={userInfo.userImage} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+                                </Upload>
                                     <AntdInput
                                         name="firstName"
                                         label="First Name"
