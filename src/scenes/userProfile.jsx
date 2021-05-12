c720db22dbf0d1c840609374f101abf6997ac73e
aperdomoll90 <aperdomoll90@gmail.com>
2021-05-12 14:37:56 -0400

    header layout done

--- src/scenes/userProfile.jsx
+++ src/scenes/userProfile.jsx
@@ -9,23 +9,35 @@ const UserProfile = () => {
   return (
     <>
     <main>
-      <section id="userProfileHeader">
-        <div id="userProfileImage">
+      <section id="userProfileHeader" className="userProfileHeaderGid">
+
+        <section id="imageBox"className="imageBox" >
+
           <div><img alt="userImage"/>{userInfo?.userImage}</div>
           <Button onClick={() => history.push(`/updateUser/${userInfo?.id}`)}>
             Edit User
           </Button>
-        </div>
-        <div>
-          {userInfo?.firstName} {userInfo?.lastName}
-        </div>
+
+        </section>
+       
+    <section id="userInfoBox" className="userInfoBox">
+
+    <div> <p> Hi: {userInfo?.firstName} {userInfo?.lastName}</p></div>
         <div>{userInfo?.company}</div>
+    <section/>
+
       </section>
-      <section id="userProfileHero">
+      </section>
+
+
+      <section id="userProfileHero" className="userProfileHero">
+      <div className="skills">
       <p>Check out my skills</p>
         <div id="userProfileSkills">
           {/* <div>{(userInfo.mySkills).join(', ')}</div> */}
         </div>
+        </div>
+
         <div id="userProfileLinks">
         <p>Links</p>
           <div><img alt="email"/>{userInfo?.email}</div>
@@ -33,11 +45,14 @@ const UserProfile = () => {
           <div><img alt="calendlyLink"/>{userInfo?.calendlyLink}</div>
         </div>
       </section>
+
+
       <section id="userProfileFooter">
           <p>Additional Information</p>
           <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quibusdam modi, quas accusantium temporibus sapiente aliquam nemo perferendis eligendi iure praesentium, tenetur dignissimos. Nulla veniam a aut illo, corporis dolorem?</div>
         {/* <div>{userInfo.additionalInformation}</div> */}
       </section>
+      
       </main>
     </>
   )
